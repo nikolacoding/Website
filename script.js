72a2a19c7568438745d70695307d595e3c7e5c18
nikolacoding <zmajic414@gmail.com>
2021-01-13 22:57:41 +0100

    beta 0.3
    - added better screen shrinking (mobile) support for most elements
    - removed email and last name from most successful users' information since the website went into a whole another direction during development than where I initially expected it to go during the time when I added all of those parameters

--- script.js
+++ script.js
@@ -1,22 +1,20 @@
-function User(first, last, age, email, balance){
+function User(first, age, balance){
     this.firstName = first;
-    this.lastName = last;
     this.age = age;
-    this.email = email;
     this.balance = "$" + balance;
     this.image = function(){
         return "Data/" + this.firstName + ".jpg";
     };
 }
 
-var User1 = new User("Marko", "Simic", 25, "maki95@gmail.com", "470.94");
-var User2 = new User("Janko", "Isidorovic", 19, "jankojanko22@gmail.com", "3571.89");
-var User3 = new User("Zoran", "Radovic", 28, "zoka92@yahoo.com", "4300.32");
-var User4 = new User("Zdravko", "Jovanovic", 32, "zdravke88@yahoo.com", "541,234.32");
-var User5 = new User("Samujilo", "Krstic", 49, "samkekrsta@yahoo.com", "1,000.000.00");
+var User1 = new User("Marko", 25, "470.94");
+var User2 = new User("Janko", 19, "3571.89");
+var User3 = new User("Zoran", 28, "4300.32");
+var User4 = new User("Zdravko", 32, "541,234.32");
+var User5 = new User("Samujilo", 49, "1,000.000.00");
 
 var Users = [User1, User2, User3, User4, User5];
-var propertyList = ["image()", "firstName", "lastName", "age", "email", "balance"]
+var propertyList = ["image()", "firstName", "age", "balance"]
 
 var temptable = "";
 var rows = propertyList.length;
